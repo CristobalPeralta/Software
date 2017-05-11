f1d43fabea465e1dc1356b0fa91efeffef3b5624
Matias Mattamala <mmattamala@ing.uchile.cl>
2017-05-11 17:32:52 -0300

    listo el segmented image

--- catkin_ws/src/ros_cap/src/segment_image.py
+++ catkin_ws/src/ros_cap/src/segment_image.py
@@ -8,12 +8,12 @@ from sensor_msgs.msg import Image
 
 from cv_bridge import CvBridge, CvBridgeError
 
-lower_blue = np.array([110,50,50])
-upper_blue = np.array([130,255,255])
+lower_blue = np.array([90,50,50])
+upper_blue = np.array([150,255,255])
 lower_red = np.array([0,0,0])
 upper_red = np.array([0,0,0])
-lower_yellow = np.array([0,0,0])
-upper_yellow = np.array([0,0,0])
+lower_yellow = np.array([20,150,120])
+upper_yellow = np.array([30,255,255])
 
 
 class SegmentImage():
@@ -22,13 +22,14 @@ class SegmentImage():
 
 
         #Subscribirce al topico "/duckiebot/camera_node/image/raw"
-        self.image_subscriber = rospy.Subscriber("/duckiebot/camera_node/image/raw", Image, process_callback)
+        self.image_subscriber = rospy.Subscriber("/duckiebot/camera_node/image/raw", Image, self._process_image)
 
         #Clase necesaria para transformar el tipo de imagen
         self.bridge = CvBridge()
 
         #Ultima imagen adquirida
         self.cv_image = Image()
+        self.base_pub = rospy.Publisher("/duckiebot/segment_image/image/raw", Image, queue_size=1)
         
 
 
@@ -44,18 +45,19 @@ class SegmentImage():
         frame = self.cv_image
 
         #Cambiar tipo de color de BGR a HSV
-        image_out = cv2.cvtColor(frame,cv2.BGR2HSV)
+        image_out = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
         # Filtrar colores de la imagen en el rango utilizando 
         #mask = cv2.inRange(image, lower_limit, upper_limit)
-        mask = cv2.inRange(image_out, lower_blue, upper_blue)
+        mask = cv2.inRange(image_out, lower_yellow, upper_yellow)
 
         # Bitwise-AND mask and original image
         segment_image = cv2.bitwise_and(frame,frame, mask= mask)
 
         #Publicar imagenes
-        image_out = cv2.cvtColor(segment_image,cv2.HSV2BGR)
-		msg = bridge.cv2_to_imgmsg(image_out, "brg8")
+        image_out = cv2.cvtColor(segment_image,cv2.COLOR_HSV2BGR)
+        msg = self.bridge.cv2_to_imgmsg(segment_image, "bgr8")
 		# Punblicar msg a topico XXXXX
+        self.base_pub.publish(msg)
 
 def main():
 
